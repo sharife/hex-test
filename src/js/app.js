e43ab780d5b9cec2cf77128675ada2e9fda4aa02
Sharif Ezzat <goodsharif@gmail.com>
2019-01-14 11:49:45 -0800

    added animation effects

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,43 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+$(function(){  // $(document).ready shorthand
+  $('.intro1').fadeIn(1500);
+  $('.intro1').addClass('scaler');
+  $('.intro2').delay(2500).fadeIn(1000);
+  $('.intro3').delay(3500).fadeIn(1000);
+});
+
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1', 'padding-top': '-10px'},500);
+                    
+            }
+        }); 
+    });
+});
+
+
+$(function(){ 
+  var $el = $(".example:first"), text = $el.text(),
+      words = text.split(" ");
+
+  var html = "";
+  for (var i = 0; i < words.length; i++) {
+      html += "<span>" + words[i] + " </span>";
+  };
+  $el.html(html).children().hide().each(function(i){
+    $(this).delay(i*500).fadeIn(700);
+  });
+});
