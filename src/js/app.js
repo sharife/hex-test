499ea287ef4cbbbb529b346061f0235ccb078948
Sharif Ezzat <goodsharif@gmail.com>
2019-05-28 21:38:30 -0700

    replaced homepage artwork

--- src/js/app.js
+++ src/js/app.js
@@ -10,8 +10,10 @@ if (window.netlifyIdentity) {
 }
 
 $(function(){  // $(document).ready shorthand
-  $('.intro1').animate({'opacity':'1', 'margin-left': '0'},700);
+  $('.intro1').animate({'opacity':'1', 'margin-left': '0'},1000);
+  $('.header-image').animate({'opacity':'1'},1000);
   // $('.intro1').addClass('scaler');
+  $('#scroll').delay(1000).animate({'opacity':'1'},700);
   $('.intro2').delay(2000).animate({'opacity':'1'},700);
   $('.intro3').delay(2500).animate({'opacity':'1'},700);
   $('.intro4').delay(2800).animate({'opacity':'1'},700);
@@ -50,3 +52,42 @@ $(function(){
     $(this).delay(i*500).fadeIn(700);
   });
 });
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 0
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          /*var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+          */
+        });
+      }
+    }
+  });
